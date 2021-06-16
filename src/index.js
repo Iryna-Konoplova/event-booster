14f4fb76cd8f16ec121112f038e2e634020248d3
KaterynaMuzhetska <74104436+KaterynaMuzhetska@users.noreply.github.com>
2021-06-17 00:00:41 +0300

    addEventListeners on input and select tags

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 
-// import searchEvent from './js/serch-event';
+import searchEvent from './js/serch-event';
 // import homePage from './js/home-page';
 // import pagination from './js/pagination';
 // import modal from './js/modal';
@@ -8,6 +8,19 @@ import './sass/main.scss';
 
 import countries from './json/countries.json';
 import selectOptionsTpl from './templates/selectOptions';
+
+const selectCountry = document.querySelector('.select');
+const inputEvent = document.querySelector('input');
+
+selectCountry.addEventListener('change', (event) => {
+  searchEvent();
+  console.log(event.target.value); 
+});
+
+inputEvent.addEventListener('input',  (event) => {
+  searchEvent();
+  console.log(event.target.value); 
+});
 
 const refs = {
   select: document.querySelector('.select')
