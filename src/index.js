ea5f8d7467181f194f860757c4bd93728fd85b5b
Iryna Konoplova <a.irina773@gmail.com>
2021-06-16 14:53:59 +0300

    Добавляет countries_json

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ import './sass/main.scss';
 // import pagination from './js/pagination';
 // import modal from './js/modal';
 // import { refs } from './js/refs';
+
+import countries from './json/countries.json';
+import selectOptionsTpl from './templates/selectOptions';
+
+const refs = {
+  select: document.querySelector('.select')
+}
+
+const optionsMarkup = createSelectorOptionsMarkup(countries);
+refs.select.insertAdjacentHTML('beforeend', optionsMarkup);
+
+function createSelectorOptionsMarkup(options) {
+  return selectOptionsTpl(options);
+}
