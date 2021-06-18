d5b4fd4eb9a246de7cff08cfc84f2d15b8b2365f
Iryna Konoplova <a.irina773@gmail.com>
2021-06-18 03:37:46 +0300

    Добавляет открытие модального окна

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -4,19 +4,25 @@ const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json';
 export default class NewsApiService {
     constructor() {
         this.searchQuery = '';
+        this.id = '';
     }
 
     fetchEmbedded() {
         // console.log(this)
-        const url = `${BASE_URL}?keyword=${this.searchQuery}&apikey=${API_KEY}`;
+        const url = `${BASE_URL}?keyword=${this.searchQuery}&size=20&apikey=${API_KEY}`;
         return  fetch(url)
                   .then(r => r.json())
                   .then(data => {
-                  // console.log(data)
-                  // console.log(data._embedded.events)
+                //   console.log(data)
+                //   console.log(data._embedded.events)
                 return data._embedded.events
         })   
     }
+
+
+
+
+
 
     get query() {
         return this.searchQuery;
@@ -24,5 +30,19 @@ export default class NewsApiService {
 
     set query(newQuery) {
         this.searchQuery = newQuery;
-    } 
+    }
+    
+    // fetchEventById() {
+    //     const id = `${BASE_URL}?id=${this.id}&apikey=${API_KEY}`;
+    //     return fetch(id)
+    //         .then(r => r.json())
+    //         .then(data => {
+    //             console.log(data)
+    //             console.log(data._embedded.events)
+    //             // return data._embedded.events
+    //         })
+    //         // .catch(() => {
+    //         //     errorFromServerById();
+    //         // });
+    // }
 }
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,25 +1,100 @@
-// import debounce from 'lodash.debounce';
-// import NewsApiService from './apiService';
-// import eventCardTpl from '../templates/event-card.hbs';
+import debounce from 'lodash.debounce';
+import NewsApiService from './apiService';
+import eventCardTpl from '../templates/event-card.hbs';
 
 
 
-// const refs = {
-//     searchInput: document.querySelector('.hero-form-field'),
-//     eventsContainer: document.querySelector('.event-card-set')
+const refs = {
+    searchInput: document.querySelector('.hero-form-field'),
+    eventsContainer: document.querySelector('.event-card-set'),
+    eventCard: document.querySelector('.event-card-list'),
+    modalOpen: document.querySelector('.backdrop')
+}
+
+const newsApiService = new NewsApiService();
+
+refs.searchInput.addEventListener('input', debounce(onSearch, 1000));
+refs.eventsContainer.addEventListener('click', onEventClick)
+
+function onSearch(e) {
+    e.preventDefault();
+
+    newsApiService.query = e.target.value.trim();
+    newsApiService.fetchEmbedded().then(appendEventsMarkup)   
+}
+
+function appendEventsMarkup(events) {
+  refs.eventsContainer.insertAdjacentHTML('beforeend', eventCardTpl(events));
+}
+
+
+
+function onEventClick(e) {
+//   evt.preventDefault();
+//   if (!evt.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+    const id = e.target.id;
+console.log(e.target.id)
+  refs.modalOpen.classList.add('is-hidden');
+
+//   imageModal.src = evt.target.dataset.source;
+//   imageModal.alt = evt.target.alt;
+
+//   buttonCloseModal.addEventListener('click', onButtonCloseModalClick)
+//   lightboxOverlay.addEventListener('click', onButtonCloseModalClick)
+//   window.addEventListener('keydown', onCloseModalEscapeKeydown)
+//   document.addEventListener('keydown', onArrowKeyPressAddSrc);
+//   document.addEventListener('keydown', onArrowKeyPressAddDescription);
+}
+
+// function onButtonCloseModalClick(evt) {
+//   modalOpen.classList.remove('is-open');
+
+//   imageModal.src = '';
+//   imageModal.alt = '';
 // }
 
-// const newsApiService = new NewsApiService();
-
-// refs.searchInput.addEventListener('input', debounce(onSearch, 1000));
-
-// function onSearch(e) {
-//     e.preventDefault();
-
-//     newsApiService.query = e.target.value.trim();
-//     newsApiService.fetchEmbedded().then(appendEventsMarkup)   
+// function onCloseModalEscapeKeydown(evt) {
+//   if (evt.code === 'Escape') {
+//     onButtonCloseModalClick();
+//   }
 // }
 
-// function appendEventsMarkup(events) {
-//   refs.eventsContainer.insertAdjacentHTML('beforeend', eventCardTpl(events));
-// }
+// function onArrowKeyPressAddSrc(evt) {
+//   let newIndex = arrayOriginalImages.indexOf(imageModal.src);
+//   if (newIndex < 0) {
+//     return;
+//   }
+//   if (evt.code === 'ArrowLeft') {
+//     newIndex -= 1;
+//     if (newIndex === -1) {
+//       newIndex = arrayOriginalImages.length - 1;
+//     }
+//   } else if (evt.code === 'ArrowRight') {
+//     newIndex += 1;
+//     if (newIndex === arrayOriginalImages.length) {
+//       newIndex = 0;
+//     }
+//   }
+//   imageModal.src = arrayOriginalImages[newIndex];
+// };
+
+// function onArrowKeyPressAddDescription(evt) {
+//     let newIndex = arrayDescriptionImages.indexOf(imageModal.alt);
+//   if (newIndex < 0) {
+//     return;
+//   }
+//   if (evt.code === 'ArrowLeft') {
+//     newIndex -= 1;
+//     if (newIndex === -1) {
+//       newIndex = arrayDescriptionImages.length - 1;
+//     }
+//   } else if (evt.code === 'ArrowRight') {
+//     newIndex += 1;
+//     if (newIndex === arrayDescriptionImages.length) {
+//       newIndex = 0;
+//     }
+//   }
+//   imageModal.alt = arrayDescriptionImages[newIndex]
+// };
