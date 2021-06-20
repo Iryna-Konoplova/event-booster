6c16697fb5d3400cf84783b41c2f1c85690f9c9d
SerhiiZadorozhnyi <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-06-20 16:39:13 +0200

    Add top button

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import searchEvent from './js/serch-event';
 import homePage from './js/home-page';
 import pagination from './js/pagination';
 import modal from './js/modal';
-
+import button_top from './js/button-top'
 
 import countries from './json/countries.json';
 import selectOptionsTpl from './templates/selectOptions';
